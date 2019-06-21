f5b3d0a28abd7864e1fa4d49c3977149de28fabe
walteng <ngetichwalt49@gmail.com>
2019-06-21 08:14:39 +0300

    adding js files for BL

---
+++ JS/scripts.js
@@ -0,0 +1,12 @@
+// BI LOGIC
+//
+//
+// var finalFarmInput = [];
+//
+// function Inquiry(size, seed, fert,location) {
+//   this.customSize = size;
+//   this.customSeed = seed;
+//   this.customfertilizer = fert;
+//   this.customPlace = location;
+//   this.farmInputs = 0;
+// }
