a5fc0fc60e15ec191a455733bc63a23f5ef9ca71
walteng <ngetichwalt49@gmail.com>
2019-06-21 11:34:09 +0300

    added interaction tasks to the page sections

--- JS/scripts.js
+++ JS/scripts.js
@@ -1,12 +1,1 @@
-// BI LOGIC
-//
-//
-// var finalFarmInput = [];
-//
-// function Inquiry(size, seed, fert,location) {
-//   this.customSize = size;
-//   this.customSeed = seed;
-//   this.customfertilizer = fert;
-//   this.customPlace = location;
-//   this.farmInputs = 0;
-// }
+
